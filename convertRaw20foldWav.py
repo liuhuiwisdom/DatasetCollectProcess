635c7f897010693a25208501605b7c4a6c4b8cdb
daleloogn <daleloogn@126.com>
2016-02-16 13:41:36 +0800

    rerun the main code

--- convertRaw20foldWav.py
+++ convertRaw20foldWav.py
@@ -205,3 +205,17 @@ def draw_wav(wav_dir):
     pylab.ylabel("wav_data")
     pylab.show()
     return 0
+
+#######################################
+# main steps
+#######################################
+# step 1
+# rename_artist_folds()
+# step 2 manually change the out to 107singersMp3 and delete the old one
+# step 3
+# combine_same_singer()
+# step 4
+# batch_split_20_folds()
+# step 5
+# split_train_test()
+# step 6 finally change the 20_fold to 107singersMp3_20_fold and we get the needed dataset
